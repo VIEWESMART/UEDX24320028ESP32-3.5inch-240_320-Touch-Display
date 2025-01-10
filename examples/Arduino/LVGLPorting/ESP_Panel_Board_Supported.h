5e95173416892c1211988eb7a88917c21d25a6b1
VIEWESMART <liulong085711@outlook.com>
2025-01-10 11:03:27 +0800

    Update ESP_Panel_Board_Supported.h

--- examples/Arduino/LVGLPorting/ESP_Panel_Board_Supported.h
+++ examples/Arduino/LVGLPorting/ESP_Panel_Board_Supported.h
@@ -65,9 +65,9 @@
 // #define BOARD_UEDX24240013_MD50E
 // #define BOARD_VIEWE_ESP_S3_Knob_LCD_21
 // #define BOARD_UEDX48480021_MD80ET
-#define BOARD_VIEWE_ESP_S3_Touch_LCD_24  
+// #define BOARD_VIEWE_ESP_S3_Touch_LCD_24  
 // #define BOARD_VIEWE_ESP_S3_Touch_LCD_28 
-// #define BOARD_VIEWE_ESP_S3_Touch_LCD_35_V1  //The resolution is 240*320
+#define BOARD_VIEWE_ESP_S3_Touch_LCD_35_V1  //The resolution is 240*320
 // #define BOARD_VIEWE_ESP_S3_Touch_LCD_35_V2     //The resolution is 320*480
 // #define BOARD_VIEWE_ESP_S3_Touch_LCD_40
 // #define BOARD_VIEWE_ESP_S3_Touch_LCD_43_V1 //The resolution is 800*480
